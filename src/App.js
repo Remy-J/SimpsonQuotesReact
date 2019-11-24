4c4f7753d10331ca61a4206e47a85d43e5fac4b1
Rémy Jacob <rjpro77@outlook.fr>
2019-11-24 17:58:53 +0100

    added props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard />
+      <QuoteCard
+        style="Right"
+        character="Neslon Muntz"
+        quote="Shoplifting is a victimless crime, like punching someone in the dark."
+        url="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+      />
+      <QuoteCard
+        style="Left"
+        character="Abe Simpson"
+        quote="Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?"
+        url="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593"
+      />
+      <QuoteCard
+        style="Right"
+        character="Homer Simpson"
+        quote="I hope I didn t brain my damage"
+        url="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+      />
     </div>
   )
 }
