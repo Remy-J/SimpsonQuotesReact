67a8a8b239c1bb1d8c9675deae9ccf74bb31cb26
Rémy Jacob <rjpro77@outlook.fr>
2019-11-24 15:55:59 +0100

    added QuoteCard component with his style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
-import Navbar from "./components/Navbar"
+import Navbar from "./components/navbar/Navbar"
+import QuoteCard from "./components/quotecard/QuoteCard"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <QuoteCard />
     </div>
   )
 }
